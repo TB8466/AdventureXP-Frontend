b8ec00e2d58cbaae9b237c843a155a6151c5fe21
Balderarrouas <balder.arrouas@gmail.com>
2021-10-14 11:52:45 +0200

    it works

--- static/js/newEmployee.js
+++ static/js/newEmployee.js
@@ -15,17 +15,21 @@ let postRequestEmployee = {
 let employeeJson = {
   "employeeId": "",
   "firstName": "",
-  "lastName": ""
+  "lastName": "",
+  "employeeType": ""
 }
 
 function createEmployee(btn){
 
-  let inpValue1 = document.querySelector(".firstNameClass");
-  let inpValue2 = document.querySelector(".lastNameClass");
+  let inpValue1 = document.querySelector(".empinp1");
+  let inpValue2 = document.querySelector(".empinp2");
+  let inpValue3 = document.querySelector(".empinp3");
 
   //Udfylder variablerne fra object
   employeeJson.firstName = inpValue1.value;
   employeeJson.lastName = inpValue2.value;
+  employeeJson.employeeType = inpValue3.value;
+
 
   //Laver json object???
   postRequestEmployee.body = JSON.stringify(employeeJson);
