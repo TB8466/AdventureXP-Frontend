3dddb3e49db1865f0a28b0b29f2c7dec4eb5f5e8
Thorbjørn <thorbjornbros@gmail.com>
2021-10-15 13:37:49 +0200

    Frontend LandingPage created

--- static/js/header.js
+++ static/js/header.js
@@ -3,8 +3,12 @@ const body = document.querySelector("body");
 function createHeader(){
   const header = document.createElement("header");
   const h1 = document.createElement("h1");
+  const goBack = document.createElement("a");
   h1.innerText = "Welcome";
   header.appendChild(h1);
+  goBack.innerText = "Go Back";
+  goBack.href = "../landingPage/home.html";
+  header.appendChild(goBack);
   body.appendChild(header);
 }
 createHeader();
