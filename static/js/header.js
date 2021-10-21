94fc311903b83418bb2e45d1b78734aad3d9f595
Elkjaer97 <elkjaer1997@gmail.com>
2021-10-21 15:01:37 +0200

    More style shit. Genialt User Interface. Bedre styling end facebook, insta og ekstra bladet.

--- static/js/header.js
+++ static/js/header.js
@@ -1,16 +1,25 @@
 const body = document.querySelector("body");
 
 function createHeader(){
-  const header = document.createElement("header");
+ /* const header = document.createElement("header");
   const h1 = document.createElement("h1");
   const goBack = document.createElement("a");
   const wtf = document.createElement("a");
+  const wtf2 = document.createElement('a');
   h1.innerText = "AdventureXP";
   header.appendChild(h1);
   goBack.innerText = "Go Back";
   goBack.href = "../landingPage/home.html";
   header.appendChild(goBack);
 
-  body.appendChild(header);
+  wtf.innerText = "Employee";
+  wtf.href = "../activity/new-activity.html";
+  header.appendChild(wtf);
+
+  wtf2.innerText = "tb";
+  wtf2.href = "../booking/new-booking.html";
+  header.appendChild(wtf2);
+
+  body.appendChild(header); */
 }
 createHeader();
